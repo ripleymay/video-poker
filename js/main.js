9d67ec5032d3f2135889aa8e1d19e501019446c4
Ripley Mayfield <ripleymay@Ripleys-MBP.attlocal.net>
2021-09-27 16:44:06 -0500

    holds working!!!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 /*----- constants -----*/
-// from card repo
+// CARD CONSTANTS - taken from class repo
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-
 const masterDeck = buildMasterDeck();
 
 const START_CREDS = 50;
@@ -28,16 +27,17 @@ let deck, hand, credits, bet, msg, round;
 /*----- cached element references -----*/
 const scoreElems = [...document.querySelectorAll('#scoreboard > ol.values')];
 const handElem = document.getElementById('hand');
+const betBtns = document.querySelectorAll('button.bet');
+const dealBtn = document.getElementById('deal-draw');
+const cashBtn = document.getElementById('cash-out');
 
 
 /*----- event listeners -----*/
-document.querySelectorAll('button.bet').forEach(button => 
+betBtns.forEach(button => 
   button.addEventListener("click", handleBet)
 );
-// not sure if this will work - should maybe be all?
-document.getElementById('deal-draw').addEventListener('click', handleDeal);
-document.getElementById('cash-out').addEventListener('click', handleCashout);
-
+dealBtn.addEventListener('click', handleDeal);
+cashBtn.addEventListener('click', handleCashout);
 handElem.addEventListener('click', handleHold);
 
 
@@ -47,9 +47,10 @@ init();
 function init() {
     hand = [{face: 'back'}, {face: 'back'}, {face: 'back'}, 
       {face: 'back'}, {face: 'back'}];
+    // hand = [];
     credits = START_CREDS;
     bet = 0;
-    round = 0;
+    round = 1;
     msg = 'Welcome!';
 
     render();
@@ -58,13 +59,16 @@ function init() {
 function render() {
     renderScoreboard();
     renderHand();
-    document.getElementById('message').innerHTML = `${msg}`;
+    document.getElementById('message').innerHTML = `testing! round ${round}`;
     document.getElementById('bet').innerHTML = `Bet ${bet}`;
     document.getElementById('credits').innerHTML = `${credits} credits`;
+    dealBtn.disabled = (!bet) ? true : false;
+    betBtns.forEach(button => 
+      button.disabled = (round === 2) ? true : false
+    );
 }
 
 function renderScoreboard() {
-  // hightlight correct bet column
   scoreElems.forEach(function(col, index) {
     col.style.backgroundColor = (index === bet - 1) ? 'rgba(255, 0, 0, 0.8)' : 'transparent';
   })
@@ -73,40 +77,64 @@ function renderScoreboard() {
 function renderHand() {
   let cardsHtml = '';
   hand.forEach(function(card, index) {
-    cardsHtml += `<div id=c${index} class="card ${card.face}">Test</div>`;
+    let hold = (card.held) ? 'held' : '';
+    cardsHtml += `<div id=${index} class="card ${card.face} ${hold}"></div>`;
   })
   handElem.innerHTML = cardsHtml;
 }
 
 function handleBet(evt) {
   // TO DO: alert player when they dont have enough credits to bet
-
   if (evt.target.id === 'one') {
-    if (bet >= MAX_BET) {
-      bet = 1;
-      credits = credits + MAX_BET - bet;
-    } else {
-      bet++;
-      credits--;
-    }
+    betOne();
   } else if (evt.target.id === 'max') {
-    credits = credits + bet - MAX_BET;
-    bet = MAX_BET;
+    betMax();
   }
 
   render();
 }
 
-function handleDeal() {
-  if (!bet) {
-    msg = 'You\'ve gotta bet first!';
-
+function betOne() {
+  if (bet >= MAX_BET) {
+    bet = 1;
+    credits = credits + MAX_BET - bet;
   } else {
+    bet++;
+    credits--;
+  }
+}
+
+function betMax() {
+  credits = credits + bet - MAX_BET;
+  bet = MAX_BET;
+}
+
+function handleDeal() {
+  if (round === 1) {
     deck = getNewShuffledDeck();
     hand = deck.slice(0, 5);
+    round++;
 
     checkForWin();
-  } 
+  } else if (round === 2) {
+    hand.forEach(function(card, index) {
+      if (!card.held) {
+        hand[index] = deck.pop();
+      }
+    });
+    round--;
+
+    checkForWin();
+    // add winnings to credits
+    bet = 0;
+  }
+
+  render();
+}
+
+function handleHold(evt) {
+  if (round != 2) return;
+  hand[evt.target.id].held = !hand[evt.target.id].held;
 
   render();
 }
@@ -115,15 +143,12 @@ function handleCashout() {
 
 }
 
-function handleHold(evt) {
-  console.log(evt.target.id);
-}
-
 function checkForWin() {
 
 }
 
-// card functions
+
+// CARD FUNCTIONS - taken from class repo
 function buildMasterDeck() {
     const deck = [];
     // Use nested forEach to generate card objects
@@ -133,7 +158,8 @@ function buildMasterDeck() {
           // The 'face' property maps to the library's CSS classes for cards
           face: `${suit}${rank}`,
           // Setting the 'value' property for game of blackjack, not war
-          value: Number(rank) || (rank === 'A' ? 11 : 10)
+          value: Number(rank) || (rank === 'A' ? 11 : 10),
+          held: false
         });
       });
     });
